c2b4590c40295c09fb88cf637064f7ce1205156d
sjwilliams <sjwilliams@a02837fa-88ba-2307-23dd-5b8dd1ba40a4>
2010-06-10 06:35:22 +0000

    moved jScrollPane out of CalPress (and into Mission Local for that site-specific functionality. Also, re-enabled google cdn version of jquery for non-admin page. Admin page needs no-conflicts mode, which default jquery will do
    git-svn-id: http://calpresstheme.googlecode.com/svn/trunk@9 a02837fa-88ba-2307-23dd-5b8dd1ba40a4

--- library/extensions/init.php
+++ library/extensions/init.php
@@ -1,19 +1,26 @@
 <?php
 function calpress_init() {
-    
-    //use google CDN jQuery
-    //wp_deregister_script( 'jquery' );
-    //wp_register_script( 'jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js');
+	
+	//load Google's jQuery if not in admin (faster CDN). 
+	//In admin, we need the default, which is in no-conflicts mode
+	if( !is_admin()){
+	   wp_deregister_script('jquery'); 
+	   wp_register_script('jquery', ("http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"), false, '1.3.2'); 
+	}
+	
+ 	//jQuery
     wp_enqueue_script('jquery');  
     
-    //SWFOject 1.5
-    wp_enqueue_script('swfobject1.5', PARENTJS . '/swfobject.js');
-    
-    //ThickBox    
-    wp_enqueue_script('thickbox');
+	if( !is_admin()){
+		//SWFOject 1.5. Default is 2.0.
+	    wp_enqueue_script('swfobject1.5', PARENTJS . '/swfobject.js');
+
+	    //ThickBox    
+	    wp_enqueue_script('thickbox');		
+	}
      
-    //Non-standard, Calpress-specific plugins and script
-    wp_enqueue_script('calpress-min', PARENTJS . '/calpress-min.js');  
+    //Non-standard, Calpress-specific plugins and scripts
+    //wp_enqueue_script('calpress-min', PARENTJS . '/calpress-min.js');  
 }    
 add_action('init', 'calpress_init');
 ?>
