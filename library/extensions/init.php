8c4e41ced629ce64b84ed1b2b988da059e7cca5d
sjwilliams <sjwilliams@a02837fa-88ba-2307-23dd-5b8dd1ba40a4>
2010-06-25 00:09:56 +0000

    changed google cdn version of jquery to most recent
    git-svn-id: http://calpresstheme.googlecode.com/svn/trunk@10 a02837fa-88ba-2307-23dd-5b8dd1ba40a4

--- library/extensions/init.php
+++ library/extensions/init.php
@@ -5,7 +5,7 @@ function calpress_init() {
 	//In admin, we need the default, which is in no-conflicts mode
 	if( !is_admin()){
 	   wp_deregister_script('jquery'); 
-	   wp_register_script('jquery', ("http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"), false, '1.3.2'); 
+	   wp_register_script('jquery', ("http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"), false, '1.4.2'); 
 	}
 	
  	//jQuery
