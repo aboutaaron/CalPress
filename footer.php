93e6724c83b1c0bb493ecfd3fb4e1b3a48f6274a
sjwilliams <sjwilliams@a02837fa-88ba-2307-23dd-5b8dd1ba40a4>
2010-07-30 04:15:27 +0000

    dom-fix ported r35 into trunk
    git-svn-id: http://calpresstheme.googlecode.com/svn/trunk@36 a02837fa-88ba-2307-23dd-5b8dd1ba40a4

--- footer.php
+++ footer.php
@@ -1,20 +1,21 @@
 
-    	<div id="footer" class="grid_12 alpha omega">
-    		<span class="copyright">Copyright <?php echo date('Y'); ?>, <?php bloginfo('name') ?></span>
-    		<span class="meta-sep">|</span>
-    		<span class="poweredby">Powered by <span id="generator-link"><a href="http://wordpress.org/" title="<?php _e( 'WordPress', 'sandbox' ) ?>" rel="generator"><?php _e( 'WordPress', 'sandbox' ) ?></a></span> and the <span id="theme-link"><a href="http://calpresstheme.org" title="<?php _e( 'CalPress theme for WordPress', 'sandbox' ) ?>" rel="designer"><?php _e( 'CalPress', 'sandbox' ) ?></a> theme.</span></span>
-    		<?php
-    		    global $calpress_mobile;
-    		    if ($calpress_mobile->showMobile()) {
-    		?>
-    		    <span class="meta-sep">|</span> 
-    		    <span class="switch-mode"><a href="?theme_view=standard">Standard site</a></span>
-    		<?php        
-    		    }
-    		?>
-    		
-    	</div><div class="clear"></div><!-- #footer -->
     </div><div class="clear"></div><!-- #container -->
+    <div id="footer" class="grid_12">
+		<span class="copyright">Copyright <?php echo date('Y'); ?>, <?php bloginfo('name') ?></span>
+		<span class="meta-sep">|</span>
+		<span class="poweredby">Powered by <span id="generator-link"><a href="http://wordpress.org/" title="<?php _e( 'WordPress', 'sandbox' ) ?>" rel="generator"><?php _e( 'WordPress', 'sandbox' ) ?></a></span> and the <span id="theme-link"><a href="http://calpresstheme.org" title="<?php _e( 'CalPress theme for WordPress', 'sandbox' ) ?>" rel="designer"><?php _e( 'CalPress', 'sandbox' ) ?></a> theme.</span></span>
+		<?php
+		    global $calpress_mobile;
+		    if ($calpress_mobile->showMobile()) {
+		?>
+		    <span class="meta-sep">|</span> 
+		    <span class="switch-mode"><a href="?theme_view=standard">Standard site</a></span>
+		<?php        
+		    }
+		?>
+		
+	</div><div class="clear"></div><!-- #footer -->
+    
 </div><!-- #wrapper .hfeed -->
 
 <?php wp_footer() ?>
