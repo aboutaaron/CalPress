98bb87e695e21db5f473aa59c59a3171639e8374
sjwilliams <sjwilliams@a02837fa-88ba-2307-23dd-5b8dd1ba40a4>
2010-07-30 15:59:19 +0000

    created hooks file
    git-svn-id: http://calpresstheme.googlecode.com/svn/trunk@42 a02837fa-88ba-2307-23dd-5b8dd1ba40a4

---
+++ library/extensions/hooks.php
@@ -0,0 +1,24 @@
+<?php
+// This file is part of the CalPress Theme for WordPress
+// http://calpresstheme.org
+//
+// CalPress is a project of the University of California 
+// Graduate School of Journalism
+// http://journalism.berkeley.edu
+//
+// Copyright (c) 2010 The Regents of the University of California
+//
+// Released under the GPL Version 2 license
+// http://www.opensource.org/licenses/gpl-2.0.php
+//
+// **********************************************************************
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+// **********************************************************************
+
+// Inspired by the work of Benedict Eastaugh: 
+// http://extralogical.net/2007/06/wphooks/
+
+
+?>
