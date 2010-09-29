2c5a25d0716ffbf0d996cb2177e62aa1d15874b8
sjwilliams <sjwilliams@a02837fa-88ba-2307-23dd-5b8dd1ba40a4>
2010-09-29 00:54:36 +0000

    news3 layout and styles
    git-svn-id: http://calpresstheme.googlecode.com/svn/trunk@141 a02837fa-88ba-2307-23dd-5b8dd1ba40a4

---
+++ layouts/news3.php
@@ -0,0 +1,168 @@
+<?php
+// Alternate new layout that ships with CalPress. This layout will automatically appear in all child themes. 
+// For use in CalPress or child themes, simply select "News2" in the layout selector in the CalPress Producer. 
+
+// This theme can be replaced with another "news2" layout by placing a custom "newss.php" file in the 
+// child theme's /layout/ folder. Or, even better, you can suppliment this theme another layout using a 
+// a different file name (eg: /layouts/breaking-news.php). Then you can select from either news layout.
+
+// Also, CalPress includes a custom body class selector to denote which layout you are currently using. 
+// The selector takes the form: front-layout-yourfilenameminusthephp. For example, this file (/layout/news2.php)
+// adds a body selector like so: body class="front-layout-news2". A custom child theme layout of /layout/breaking-news.php
+// would do this: body class="front-layout-breaking-news"
+
+// This file makes extensive use of the calpress_loop_content function. This is the default CalPress loop, and it lives in 
+// /loops/loop-content.php. The function has the following signature and default values:
+//
+// calpress_loop_content($art=true, $artsize=620, $artcrop=0, $multimedia=true, $hed=true, $meta=true, $excerpt=true, $excerptlength = 0)
+//
+// And the functions documentation:
+//
+// @param boolean $art - show lead story art if available
+// @param boolean $arttease - show full fledge multimedia options (video, soundslides, youtube, etc). If false, just an image will appear
+// @param int $artsize - width of art. The default is 620px, which is full column lead story size
+// @param boolean $hed - show headline
+// @param boolean $meta - show story meta
+// @param boolean $excerpt - show story excerpt
+// @param int $excerptlength - for any number greater than zero, truncate excerpt to that length
+//  @return string
+?>
+
+<?php $storyCounter = 0 ?>
+
+<?php // Only show news design page on the very front page. Once we start paging, go to a basic view ?>
+<?php if (!is_paged()): ?>
+    
+    <?php
+    
+    // default loop needs to be shortened by number of features shown
+    $maxpost = get_option('posts_per_page');
+    
+    if ($use_front_feature){
+        $maxpost = $maxpost - 1;
+    }
+    
+    ?>
+    
+    <?php while ( have_posts() ) : the_post() ?>
+
+        <?php if ($storyCounter === 0): // lead story ?>
+            <div id="lead-story">
+            
+            <?php if ($leadstoryoverride): // calpress producer code ?>
+                
+                <div class="hentry p1 post publish category-front front-override">
+                  <?php  echo(stripslashes($leadstoryoverride_content)); ?>
+                </div>
+            
+            <?php else: // automated featured story ?> 
+
+                <?php  
+                    if ($use_front_feature){ // use featured + front loop's content
+                        // tmp save global post
+                        $tmpGlobalPost = $post;
+                        // make global post the first featured story                        
+                        $post = get_post($featuredfrontposts_ids[$storyCounter]);
+                    } 
+                ?>
+                    <?php calpress_loop_content(true, 300, 0, true, true, true, true, 40); ?>
+                    
+                <?php
+                    if ($use_front_feature) {
+                        // reassign old $post global back
+                        $post = $tmpGlobalPost;
+                    }
+                ?>
+            
+            <?php endif ?>    
+            
+            </div><!-- #lead story -->
+        <?php elseif ($storyCounter === 1 ): // story 2 ?>
+            
+            
+                
+        <div id="secondary-stories">
+            
+            <?php
+            if ($use_front_feature){ // use featured + front loop's content
+                // tmp save global post
+                $tmpGlobalPost = $post;
+                // make global post the first featured story                        
+                $post = get_post($featuredfrontposts_ids[$storyCounter]);
+            }
+            ?>
+            
+            <?php calpress_loop_content(true, 300, 200, true, true, true, true, 15); ?>
+            
+            <?php
+                if ($use_front_feature) {
+                    // reassign old $post global back
+                    $post = $tmpGlobalPost;
+                }
+            ?>
+
+        <?php elseif ($storyCounter >= 2 && $storyCounter <= 3): //story 3 - 4 ?>
+
+            <?php
+            if ($use_front_feature){ // use featured + front loop's content
+                // tmp save global post
+                $tmpGlobalPost = $post;
+                // make global post the first featured story                        
+                $post = get_post($featuredfrontposts_ids[$storyCounter]);
+            }
+            ?>
+
+            <?php calpress_loop_content(true, 300, 200, true, true, true, true, 15); ?>
+            
+        <?php elseif ($storyCounter === 4): //story 5 ?>
+        
+            <?php calpress_loop_content(true, 300, 200, true, true, true, true, 15); ?>
+        
+            <?php
+                if ($use_front_feature) {
+                    // reassign old $post global back
+                    $post = $tmpGlobalPost;
+                    rewind_posts();
+                }
+            ?>
+
+        </div><!-- #secondary-stories -->
+        <div id="tertiary-stories">         
+        <?php elseif ($storyCounter >= 4 && $storyCounter < $maxpost + 1): ?>
+            <?php if ($storyCounter === 5): ?>
+                <?php calpress_loop_content(false, 0, 0, false,true,true,true, 0); ?>
+            <?php else: ?>
+                <?php calpress_loop_content(false, 0, 0, false,true,false,false, 0); ?>
+            <?php endif ?>
+            
+        <?php endif; ?>
+
+<?php $storyCounter++ ?>
+<?php endwhile; ?>
+</div><!-- #tertiary-stories -->
+            
+            <div id="nav-below" class="navigation">
+            	<div class="nav-previous"><?php next_posts_link(__( 'Older posts <span class="meta-nav">&raquo;</span>', 'sandbox' )) ?></div>
+            	<div class="nav-next"><?php previous_posts_link(__( '<span class="meta-nav">&laquo;</span>Newer posts', 'sandbox' )) ?></div>
+            </div>
+        
+
+<?php else: ?>    
+    
+    <div id="nav-above" class="navigation">
+    	<div class="nav-previous"><?php next_posts_link(__( 'Older posts <span class="meta-nav">&raquo;</span>', 'sandbox' )) ?></div>
+    	<div class="nav-next"><?php previous_posts_link(__( '<span class="meta-nav">&laquo;</span>Newer posts', 'sandbox' )) ?></div>
+    </div>
+
+    <?php while ( have_posts() ) : the_post() ?>
+        <?php // show post with art, sized at 620px ?>
+        <?php calpress_loop_content(); ?>
+
+    <?php endwhile; ?>
+
+    <div id="nav-below" class="navigation">
+    	<div class="nav-previous"><?php next_posts_link(__( 'Older posts <span class="meta-nav">&raquo;</span>', 'sandbox' )) ?></div>
+    	<div class="nav-next"><?php previous_posts_link(__( '<span class="meta-nav">&laquo;</span>Newer posts', 'sandbox' )) ?></div>
+    </div>
+    
+<?php endif ?>
