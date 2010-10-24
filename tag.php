ec9b0a628ce268d5c7c8ea04761be105440ace31
sjwilliams <sjwilliams@a02837fa-88ba-2307-23dd-5b8dd1ba40a4>
2010-10-24 23:48:42 +0000

    for tag pages, show full art
    git-svn-id: http://calpresstheme.googlecode.com/svn/trunk@159 a02837fa-88ba-2307-23dd-5b8dd1ba40a4

--- tag.php
+++ tag.php
@@ -18,35 +18,16 @@
             </div>
             
 
-<?php while ( have_posts() ) : the_post() ?>
+            <?php $storyCounter = 0; while ( have_posts() ) : the_post() ?>
 
-			<div id="post-<?php the_ID() ?>" class="<?php calpress_post_class() ?>">
-				<h3 class="entry-title"><a href="<?php the_permalink() ?>" title="<?php printf( __( 'Permalink to %s', 'sandbox' ), the_title_attribute('echo=0') ) ?>" rel="bookmark"><?php the_title() ?></a></h3>
-				<div class="entry-date"><abbr class="published" title="<?php the_time('Y-m-d\TH:i:sO') ?>"><?php unset($previousday); printf( __( '%1$s &#8211; %2$s', 'sandbox' ), the_date( '', '', '', false ), get_the_time() ) ?></abbr></div>
-				<div class="entry-content">
-<?php the_excerpt(__( 'Read More <span class="meta-nav">&raquo;</span>', 'sandbox' )) ?>
+                        <?php calpress_loop_content(true, 380, 0, false, true, true, true, 25); ?>
 
-				</div>
-				<div class="entry-meta">
-					<span class="author vcard"><?php printf( __( 'By %s', 'sandbox' ), '<a class="url fn n" href="' . get_author_link( false, $authordata->ID, $authordata->user_nicename ) . '" title="' . sprintf( __( 'View all posts by %s', 'sandbox' ), $authordata->display_name ) . '">' . get_the_author() . '</a>' ) ?></span>
-					<span class="meta-sep">|</span>
-					<span class="cat-links"><?php printf( __( 'Posted in %s', 'sandbox' ), get_the_category_list(', ') ) ?></span>
-					<span class="meta-sep">|</span>
-<?php if ( $tag_ur_it = sandbox_tag_ur_it(', ') ) : // Returns tags other than the one queried ?>
-					<span class="tag-links"><?php printf( __( 'Also tagged %s', 'sandbox' ), $tag_ur_it ) ?></span>
-					<span class="meta-sep">|</span>
-<?php endif; ?>
-<?php edit_post_link( __( 'Edit', 'sandbox' ), "\t\t\t\t\t<span class=\"edit-link\">", "</span>\n\t\t\t\t\t<span class=\"meta-sep\">|</span>\n" ) ?>
-					<span class="comments-link"><?php comments_popup_link( __( 'Comments (0)', 'sandbox' ), __( 'Comments (1)', 'sandbox' ), __( 'Comments (%)', 'sandbox' ) ) ?></span>
-				</div>
-			</div><!-- .post -->
+            <?php $storyCounter++; endwhile; ?>
 
-<?php endwhile; ?>
-
-			<div id="nav-below" class="navigation">
-				<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&laquo;</span> Older posts', 'sandbox' ) ) ?></div>
-				<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&raquo;</span>', 'sandbox' ) ) ?></div>
-			</div>
+            <div id="nav-below" class="navigation">
+            	<div class="nav-previous"><?php next_posts_link(__( 'Older posts <span class="meta-nav">&raquo;</span>', 'sandbox' )) ?></div>
+            	<div class="nav-next"><?php previous_posts_link(__( '<span class="meta-nav">&laquo;</span>Newer posts', 'sandbox' )) ?></div>
+            </div>
 
 		</div><!-- #content -->
         <?php get_sidebar() ?>
