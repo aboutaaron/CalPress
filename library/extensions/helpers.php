39a351a06d63ca312fe6443b20c7dbe55824b971
jeremy.rue@gmail.com <jeremy.rue@gmail.com@a02837fa-88ba-2307-23dd-5b8dd1ba40a4>
2011-05-10 20:16:13 +0000

    Adding new asynchronous Google tracking code
    git-svn-id: http://calpresstheme.googlecode.com/svn/trunk@190 a02837fa-88ba-2307-23dd-5b8dd1ba40a4

--- library/extensions/helpers.php
+++ library/extensions/helpers.php
@@ -62,21 +62,22 @@ function calpress_googleanalytics() {
     
     // see if its value is not blank
     if ($gaID != ""){
-        $gacode = '<script type="text/javascript">
-        var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-        document.write(unescape("%3Cscript src=\'" + gaJsHost + "google-analytics.com/ga.js\' type=\'text/javascript\'%3E%3C/script%3E"));
-        </script>
-        <script type="text/javascript">
-        try {
-        var pageTracker = _gat._getTracker("'. $gaID . '");
-        pageTracker._trackPageview();
-        } catch(err) {}
-        </script>';
+	$gacode = '<!-- GOOGLE ANALYTICS TRACKING CODE -->
+	<script type="text/javascript">
+		var _gaq = _gaq || [];
+		_gaq.push([\'_setAccount\', \''. $gaID . '\']);
+		_gaq.push([\'_trackPageview\']);
 
-        echo $gacode;   
+		(function() {
+		var ga = document.createElement(\'script\'); ga.type = \'text/javascript\'; ga.async = true;
+		ga.src = (\'https:\' == document.location.protocol ? \'https://ssl\' : \'http://www\') + \'.google-analytics.com/ga.js\';
+		var s = document.getElementsByTagName(\'script\')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+	</script>'."\n";
+        echo $gacode;
     }
 }
-add_action('wp_footer', 'calpress_googleanalytics');
+add_action('wp_head', 'calpress_googleanalytics');
 
 /**
  * Display ShareThis Code to the top of posts if id set in admin
